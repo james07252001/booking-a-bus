2428c925e0d94fce9fd71f2a62be43f57bb96fa4
james07252001 <pastorillo.james25@gmail.com>
2024-12-02 13:42:45 +0800

    commit

--- admin/backend/reset_attempts.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Reset login attempts and last attempt time
-$_SESSION['login_attempts'] = 0;
-$_SESSION['last_attempt_time'] = 0;
-
-echo json_encode(['statusCode' => 200, 'title' => 'Login attempts reset']);
-?>
