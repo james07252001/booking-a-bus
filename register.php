8bc3e38523ed84d31f6ce65b3f12e10e74d5fc76
james07252001 <pastorillo.james25@gmail.com>
2024-12-16 02:18:21 +0800

    commit

--- register.php
+++ register.php
@@ -264,7 +264,7 @@ body {
     display: block;
     position: relative; /* Required for the glow effect */
     overflow: hidden;
-    width: 150px;
+    width: 1500px;
 }
 
 /* Glowing LED border effect */
