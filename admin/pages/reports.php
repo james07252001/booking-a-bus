f2807736550d188715c1ce4a0a7aa14fef0e01c2
james07252001 <pastorillo.james25@gmail.com>
2025-01-02 11:32:25 +0800

    commit

--- admin/pages/reports.php
+++ admin/pages/reports.php
@@ -195,6 +195,29 @@ if (substr($request, -4) == '.php') {
             </tbody>
         </table>
     </div>
+
+    <nav>
+        <ul class="pagination justify-content-center">
+            <?php if ($page > 1): ?>
+                <li class="page-item">
+                    <a class="page-link" href="?page=<?php echo $page - 1; ?>">Previous</a>
+                </li>
+            <?php endif; ?>
+
+            <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                <li class="page-item <?php echo ($i == $page) ? 'active' : ''; ?>">
+                    <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                </li>
+            <?php endfor; ?>
+
+            <?php if ($page < $totalPages): ?>
+                <li class="page-item">
+                    <a class="page-link" href="?page=<?php echo $page + 1; ?>">Next</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+
 </div>
 
 <!-- Google Charts script -->
